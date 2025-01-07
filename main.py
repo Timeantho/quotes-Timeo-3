e4acc706c65c0fcf244c2dbb78b302e3293819bb
Jessy33-EFREI <jessy.pertus@efrei.net>
2025-01-07 18:09:28 +0100

    update main.py

--- main.py
+++ main.py
@@ -25,3 +25,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+def display_quotes(quotes, count):
+    if count >= len(quotes):
+        print("All Quotes:")
+        view_quotes(quotes)
+    else:
+        print(f"First {count} Quotes:")
+        for i in range(count):
+            print_quote(quotes[i])
